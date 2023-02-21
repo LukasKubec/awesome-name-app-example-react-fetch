b9f61a3f10b8a79d536437c901f6938cb581905f
LukasKubec <devian3@gmail.com>
2023-02-21 10:29:13 +0100

    App vith both examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, CssBaseline, Grid } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Header } from "./sharedComponents";
+import { WithContextView, WithHooksProps } from "./views";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => {
+    return (
+        <>
+            <CssBaseline />
+            <Container maxWidth="xl">
+                <Grid container spacing={2} alignItems="center" justifyContent="space-between">
+                    <Grid item xs={12}>
+                        <Header />
+                    </Grid>
+
+                    <WithContextView />
+
+                    <WithHooksProps />
+                </Grid>
+            </Container>
+        </>
+    );
+};
 
 export default App;
